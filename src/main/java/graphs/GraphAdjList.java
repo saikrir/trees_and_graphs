499c5560894cf344e6d4febcc2319a4290e25162
Sai Katterishetty <skrao@localhost.localdomain>
2021-07-04 17:25:14 -0500

    Added Gitignore

--- src/main/java/graphs/GraphAdjList.java
+++ src/main/java/graphs/GraphAdjList.java
@@ -9,11 +9,18 @@ import java.util.stream.Collectors;
 public class GraphAdjList<T extends Comparable<T>> implements IGraph<T> {
 
 
+    private final List<Node<T>> visitedNodes;
+    private final Stack<Node<T>> traversalStack;
+    private final Queue<Node<T>> unexploredQueue;
     private final Map<Node<T>, List<Node<T>>> graphStore;
 
     public GraphAdjList() {
         this.graphStore = new HashMap<>();
+        this.visitedNodes = new ArrayList<>();
+        this.traversalStack = new Stack<>();
+        this.unexploredQueue = new ArrayDeque<>();
     }
+
 
     @Override
     public Node<T> addNode(T value) {
@@ -58,7 +65,66 @@ public class GraphAdjList<T extends Comparable<T>> implements IGraph<T> {
         return false;
     }
 
-    private Optional<Node<T>> findNode(T value) {
+    public void performDFSR(Node<T> aNode) {
+
+        if (visitedNodes.contains(aNode)) {
+            return;
+        }
+
+        log.info("Visiting {}", aNode.getValue());
+        this.visitedNodes.add(aNode);
+        this.traversalStack.push(aNode);
+
+        List<Node<T>> children = this.graphStore.get(aNode);
+        if (children.isEmpty()) {
+            this.traversalStack.pop();
+        } else {
+            for (Node<T> childNode : children) {
+                performDFS(childNode);
+            }
+        }
+    }
+
+    public void performDFS(Node<T> aNode) {
+        traversalStack.push(aNode);
+        while (!traversalStack.isEmpty()) {
+            Node<T> someNode = traversalStack.pop();
+            if (visitedNodes.contains(someNode)) continue;
+            visitedNodes.add(someNode);
+            log.info("Visited {} ", someNode.getValue());
+            this.graphStore.get(someNode).stream().filter(tNode -> !visitedNodes.contains(tNode)).forEach(node -> traversalStack.push(node));
+        }
+    }
+
+
+    public boolean find(Node<T> fNode, Node<T> toNode) {
+        traversalStack.push(fNode);
+        while (!traversalStack.isEmpty()) {
+            Node<T> someNode = traversalStack.pop();
+            if (visitedNodes.contains(someNode)) continue;
+            visitedNodes.add(someNode);
+            if(someNode.getValue().compareTo(toNode.getValue()) == 0) {
+                return true;
+            }else {
+                this.graphStore.get(someNode).stream().filter(tNode -> !visitedNodes.contains(tNode)).forEach(node -> traversalStack.push(node));
+            }
+        }
+        return false;
+    }
+
+    public void performBFS(Node<T> aNode) {
+        unexploredQueue.add(aNode);
+        while (unexploredQueue.peek() != null) {
+            Node<T> someNode = unexploredQueue.remove();
+            if (visitedNodes.contains(someNode)) continue;
+            visitedNodes.add(someNode);
+            log.info("Visited {} ", someNode.getValue());
+            List<Node<T>> adjacentList = this.graphStore.get(someNode);
+            adjacentList.forEach(tNode -> unexploredQueue.add(tNode));
+        }
+    }
+
+    public Optional<Node<T>> findNode(T value) {
         return this.graphStore.keySet().stream().filter(node -> node.getValue().compareTo(value) == 0).findAny();
     }
 
@@ -68,6 +134,10 @@ public class GraphAdjList<T extends Comparable<T>> implements IGraph<T> {
             String joinedNodes = nodes.stream().map(connectedNode -> connectedNode.getValue().toString()).collect(Collectors.joining(","));
             log.info("Node {} is connected to [ {} ]", tNode.toString(), joinedNodes);
         });
+    }
+
+    public Optional<Node<T>> firstNode() {
+        return this.graphStore.keySet().stream().findFirst();
     }
 
 }
--- src/main/java/graphs/RunGraph.java
+++ src/main/java/graphs/RunGraph.java
@@ -1,12 +1,37 @@
 package graphs;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Optional;
+
+@Slf4j
 public class RunGraph {
     public static void main(String[] args) {
-        GraphAdjList graph = new GraphAdjList();
+
+        String nodeToStart = "Sai";
+        GraphAdjList<String> graph = sample1();
+
+        Optional<Node<String>> optional = graph.findNode(nodeToStart);
+        if(optional.isEmpty()){
+            System.out.println("No Nodes yet");
+        }else{
+            System.out.println("BFS Starting " +optional.get() );
+            long start = System.currentTimeMillis();
+            System.out.println("Are Related ? " + graph.find(optional.get(), graph.findNode("Ishita").get() ));
+
+            log.info("Finished in {} ms ", System.currentTimeMillis() - start);
+        }
+
+    }
+
+    protected static GraphAdjList<String> sample1() {
+
+        GraphAdjList<String> graph = new GraphAdjList<>();
         Node sai = graph.addNode("Sai");
         Node jaya = graph.addNode("Jaya");
         Node mrinal = graph.addNode("Mrinal");
-        Node shiva = graph.addNode("shiva");
+        Node shiva = graph.addNode("Shiva");
+        Node ishita = graph.addNode("Ishita");
 
         graph.addEdge(sai, jaya);
         graph.addEdge(sai, mrinal);
@@ -15,10 +40,37 @@ public class RunGraph {
         graph.addEdge(jaya, shiva);
         graph.addEdge(mrinal, shiva);
 
-        graph.printGraph();
+        return graph;
+    }
 
-        graph.removeNode(shiva);
 
-        graph.printGraph();
+    protected static GraphAdjList<String> sample2() {
+
+        GraphAdjList<String> graph = new GraphAdjList<>();
+        Node one = graph.addNode(String.valueOf(1));
+        Node two = graph.addNode(String.valueOf(2));
+        Node three = graph.addNode(String.valueOf(3));
+        Node four = graph.addNode(String.valueOf(4));
+        Node five = graph.addNode(String.valueOf(5));
+        Node six = graph.addNode(String.valueOf(6));
+        Node seven = graph.addNode(String.valueOf(7));
+        Node eight = graph.addNode(String.valueOf(8));
+        Node nine = graph.addNode(String.valueOf(9));
+        Node ten = graph.addNode(String.valueOf(10));
+        Node eleven = graph.addNode(String.valueOf(11));
+
+
+        graph.addEdge(one, two);
+        graph.addEdge(one, three);
+        graph.addEdge(one, four);
+        graph.addEdge(two, five);
+        graph.addEdge(five, nine);
+        graph.addEdge(five, eleven);
+        graph.addEdge(three, six);
+        graph.addEdge(three, seven);
+        graph.addEdge(six, ten);
+        graph.addEdge(four, eight);
+
+        return graph;
     }
 }
